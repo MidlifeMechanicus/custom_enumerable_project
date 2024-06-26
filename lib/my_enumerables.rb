deb6968cdb8d0f61fa54fe192478af4231fe5a13
ElectronDragon <electricdragondevelopment@gmail.com>
2024-06-26 10:36:35 +0100

    Create my_map

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,33 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+  end
+
+  def my_select
+  end
+
+  def my_all?
+  end
+
+  def my_any?
+  end
+
+  def my_none?
+  end
+
+  def my_count
+  end
+
+  def my_map
+    new_array = []
+    for n in self
+      new_array << yield(n)
+    end
+    new_array
+  end
+
+  def my_inject
+  end
 end
 
 # You will first have to define my_each
